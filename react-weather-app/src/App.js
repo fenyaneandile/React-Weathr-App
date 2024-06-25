6b29d02e52f31e4d30c116648587b507572b5101
fenyaneandile <fenyaneandile@gmail.com>
2024-06-25 12:09:49 +0200

    Updated url

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://www.andile.io" target="_blank">Andile Fenyane</a> and is {""}
+        <a
+          href="https://github.com/fenyaneandile/React-Weathr-App"
+          target="_blank"
+        >
+          Open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
