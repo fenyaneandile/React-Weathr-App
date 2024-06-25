8141ac031b00e0046a241c7d58e5353df0b20224
fenyaneandile <fenyaneandile@gmail.com>
2024-06-25 20:24:30 +0200

    Added bootstrap

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -4,16 +4,23 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="https://www.andile.io" target="_blank">Andile Fenyane</a> and is {""}
-        <a
-          href="https://github.com/fenyaneandile/React-Weathr-App"
-          target="_blank"
-        >
-          Open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">h</button>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.andile.io" target="_blank">
+            Andile Fenyane
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/fenyaneandile/React-Weathr-App"
+            target="_blank"
+          >
+            Open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
